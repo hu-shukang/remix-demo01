bfea61adf749c999cc91df790361a49d3de95940
胡书康 <hushukang@gmail.com>
2024-09-26 14:48:33 +0900

    change deploy settings

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 rm -rf ./.amplify-hosting
-rm -rf node_modules
-npm install --omit=dev
 mkdir -p ./.amplify-hosting/compute/default
 
 cp -r ./build ./.amplify-hosting/compute/default/build
+cp -r server.js ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r package.json ./.amplify-hosting/compute/default/package.json
 cp -r public ./.amplify-hosting/static
-
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+cd ./.amplify-hosting/compute/default
+pwd
+npm install --omit=dev
+ls -la
+cd -
+pwd
