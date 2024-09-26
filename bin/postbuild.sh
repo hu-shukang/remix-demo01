0468c9524823f0d3468cc47182d1ce7b1ec53630
胡书康 <hushukang@gmail.com>
2024-09-26 13:54:38 +0900

    change deploy settings

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -6,7 +6,7 @@ mkdir -p ./.amplify-hosting/compute/default
 
 cp -r ./build ./.amplify-hosting/compute/default/build
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-
+cp -r package.json ./.amplify-hosting/compute/default/package.json
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
