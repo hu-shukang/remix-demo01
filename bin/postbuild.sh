da409d41cc4ef534101fb140c1218ac9251b47d3
胡书康 <hushukang@gmail.com>
2024-09-27 15:35:53 +0900

    delete express

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,9 +2,9 @@
 
 rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute/default
+mkdir -p ./.amplify-hosting/compute/default/build
 
-cp -r ./build ./.amplify-hosting/compute/default/build
-cp -r server.js ./.amplify-hosting/compute/default
+cp -r ./build/server ./.amplify-hosting/compute/default/build/server
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r package.json ./.amplify-hosting/compute/default/package.json
 cp -r ./build/client ./.amplify-hosting/static
