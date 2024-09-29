3bee9d187d5919354c9295ebba2b579e80d23355
胡书康 <hushukang@gmail.com>
2024-09-29 10:29:34 +0900

    add env

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -7,6 +7,7 @@ cp -r ./build ./.amplify-hosting/compute/default/build
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r package.json ./.amplify-hosting/compute/default/package.json
 cp -r ./server/index.js ./.amplify-hosting/compute/default/index.js
+cp -r ./server/env ./.amplify-hosting/compute/default/env
 cp -r ./build/client ./.amplify-hosting/static
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
 
