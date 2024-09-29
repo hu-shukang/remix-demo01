141d549bee237c5119bd9df5de50926693e1622d
胡书康 <hushukang@gmail.com>
2024-09-29 11:15:49 +0900

    add env

--- server/index.ts
+++ server/index.ts
@@ -5,12 +5,27 @@ import sourceMapSupport from "source-map-support";
 import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 sourceMapSupport.install();
 
-const env = fs.readFileSync(path.resolve(process.cwd(), `env/current`), 'utf8');
+// 从 `import.meta.url` 获取当前文件的路径
+const __filename = fileURLToPath(import.meta.url);
+
+// 获取当前文件所在目录的路径
+const __dirname = path.dirname(__filename);
+
+try {
+  // 同步读取文件夹下的所有文件
+  const files = fs.readdirSync(__dirname);
+  console.log('Files in current directory:', files);
+} catch (err) {
+  console.error('Error reading directory:', err);
+}
+const env = fs.readFileSync(path.resolve(__dirname, `env/current`), 'utf8');
 console.log('env', env);
 console.log('process.env.ENV', process.env.ENV);
-dotenv.config({path: path.resolve(process.cwd(), `env/.env.${env}`)});
+dotenv.config({path: path.resolve(__dirname, `env/.env.${env}`)});
 const app = fastify();
 
 const mode = env === "local" ? "development" : "production";
