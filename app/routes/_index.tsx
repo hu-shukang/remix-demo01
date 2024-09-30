01ef137516879e832001b5a5007c5661daa11b1e
胡书康 <hushukang@gmail.com>
2024-09-30 19:15:48 +0900

    change generateClient

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export const meta: MetaFunction = () => {
 };
 
 export const loader: LoaderFunction = async ({ context }) => {
-  const client = generateClient<Schema>({authMode: 'iam'});
+  const client = generateClient<Schema>();
   const { data: todos, errors } = await client.models.Todo.list();
   console.log(todos);
   console.log(errors);
